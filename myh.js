b1682f85457bffcde8634b80ce9c14030bd8410f
tfcs46 <tfcs46@yahoo.com.tw>
2025-09-10 20:54:53 +0800

    upload all files
    上傳所有檔案，不包含資料夾

---
+++ myh.js
@@ -0,0 +1,9 @@
+//建立函數，改變標題的色彩
+function changeHead(){
+    document.getElementById('myh').style.color = "pink";
+}
+
+//建立函數，還原標題的色彩
+function clearHead(){
+    document.getElementById('myh').style.color = "beige";
+}
